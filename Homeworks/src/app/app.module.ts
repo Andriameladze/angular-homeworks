0724826971a4d820093f06c6adde45671e39e571
Andria Meladze <andriameladze1@gmail.com>
2022-08-09 12:54:18 +0400

    wrote some code

--- Homeworks/src/app/app.module.ts
+++ Homeworks/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { HttpComponent } from './http/http.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginFormComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
+  declarations: [AppComponent, LoginFormComponent, HttpComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
