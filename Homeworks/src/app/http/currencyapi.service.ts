1fca51d2d8758c8c6a981adf7bea8e4f049f1369
Andria Meladze <andriameladze1@gmail.com>
2022-08-09 17:37:57 +0400

    wrote some code

--- Homeworks/src/app/http/currencyapi.service.ts
+++ Homeworks/src/app/http/currencyapi.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CurrencyapiService {
   constructor(private http: HttpClient) {}
+
+  public currency1: string = '';
+  public currency2: string = '';
+
+  private baseUrl =
+    'https://v6.exchangerate-api.com/v6/3bd1e91fcbe48670a8cd7c9e/pair/EUR/GBP';
+
+  public getCurrency(): Observable<any> {
+    return this.http.get(this.baseUrl);
+  }
 }
--- Homeworks/src/app/http/http.component.ts
+++ Homeworks/src/app/http/http.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CurrencyapiService } from './currencyapi.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
+import { AnimateTimings } from '@angular/animations';
 
 @Component({
   selector: 'app-http',
   templateUrl: './http.component.html',
-  styleUrls: ['./http.component.scss']
+  styleUrls: ['./http.component.scss'],
 })
 export class HttpComponent implements OnInit {
+  constructor(private currencyService: CurrencyapiService) {}
 
-  constructor() { }
+  public data: any = new BehaviorSubject('');
+
+  input1: any = new BehaviorSubject('');
+  input2: any = new BehaviorSubject('');
+  currency1: any;
+  currency2: any;
 
   ngOnInit(): void {
+    this.data.subscribe({
+      next: (v: any) => console.log(this.input1),
+      complete: () => console.log('Completed'),
+    });
+
+    // console.log(this.input1);
   }
 
+  // public onClick() {
+  //   this.currencyService
+  //     .getCurrency()
+  //     .pipe(tap((response) => (this.test = response)))
+  //     .subscribe();
+  // }
 }
