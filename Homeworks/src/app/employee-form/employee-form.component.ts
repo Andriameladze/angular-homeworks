bebc825868dddbab63d7d62a67527d7580cca84c
Andria Meladze <andriameladze1@gmail.com>
2022-08-11 18:45:51 +0400

    wrote some code

--- Homeworks/src/app/employee-form/employee-form.component.ts
+++ Homeworks/src/app/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-employee-form',
@@ -11,7 +13,11 @@ export class EmployeeFormComponent implements OnInit {
 
   employees: any[] = [];
 
-  constructor() {}
+  // employees = new BehaviorSubject(<any>[]);
+
+  employeeUrl: string = 'http://localhost:3000/employees';
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.formGroup = new FormGroup({
@@ -21,13 +27,42 @@ export class EmployeeFormComponent implements OnInit {
     });
   }
 
-  public onClick() {
-    this.employees.push({
-      name: this.formGroup.get('name').value,
-      salary: this.formGroup.get('salary').value,
-      age: this.formGroup.get('age').value,
-    });
+  counter = 1;
 
-    console.log(this.employees[0]);
+  postId = '';
+
+  onClick() {
+    // this.http
+    //   .post(this.employeeUrl, {
+    //     name: this.formGroup.get('name').value,
+    //     salary: this.formGroup.get('salary').value,
+    //     age: this.formGroup.get('age').value,
+    //   })
+    //   .subscribe();
+
+    this.http
+      .post(this.employeeUrl, this.formGroup.value)
+      .subscribe((data) => console.log(data));
+
+    // console.log(this.http.post(this.employeeUrl, this.formGroup.value));
+
+    this.http
+      .get(this.employeeUrl)
+      .subscribe((res) => this.employees.push(res));
+
+    // this.employees.push({
+    //   name: this.formGroup.get('name').value,
+    //   salary: this.formGroup.get('salary').value,
+    //   age: this.formGroup.get('age').value,
+    // });
+
+    // this.employees.push(
+    //   this.http
+    //     .get(this.employeeUrl)
+    //     .subscribe((res) => this.employees.push(res+"/" + ))
+    // );
+    // console.log(this.employees);
+
+    this.counter++;
   }
 }
