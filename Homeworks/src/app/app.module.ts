e5ceff2f32713e0f34f286ad7fcc6d0ede888c24
Andria Meladze <andriameladze1@gmail.com>
2022-08-12 17:17:09 +0400

    Finish Project

--- Homeworks/src/app/app.module.ts
+++ Homeworks/src/app/app.module.ts
@@ -8,15 +8,22 @@ import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { HttpComponent } from './http/http.component';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
-  declarations: [AppComponent, LoginFormComponent, HttpComponent, EmployeeFormComponent],
+  declarations: [
+    AppComponent,
+    LoginFormComponent,
+    HttpComponent,
+    EmployeeFormComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
+    NgxPaginationModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
